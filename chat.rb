65a2dac9b6c28595a3dce1cd34d93ab8889b8763
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-06-24 16:57:38 -0400

    Add files via upload

---
+++ chat.rb
@@ -0,0 +1,16 @@
+require 'programr'
+
+brains = Dir.glob("_imaginedpath/brains/*")
+
+robot = ProgramR::Facade.new
+robot.learn(brains)
+
+begin
+  print "YOU >> "
+  s = STDIN.gets.chomp
+  
+  if s != 'quit'
+    reaction = robot.get_reaction(s)
+    STDOUT.puts "ALUCA << #{reaction}"
+  end
+end while s != 'quit'
---
+++ think.rb
@@ -0,0 +1,23 @@
+# Make sure that story elements are kept on seperate lines.
+character_fate = File.readlines("_narratives/outcomes/character_fates.txt")
+dating_outcome = File.readlines("_narratives/outcomes/dating_outcomes.txt")
+
+# Make the total imagined branch the size of the darkest path.
+# branch_size = branch_1.size.to_i
+
+aiml = "<?xml version = "1.0" encoding = 'UTF-8'?>
+<aiml version = "1.0.1" encoding = 'UTF-8'>
+  <category>
+    <pattern>Can you tell me the nuetral path within this dataset?</pattern>
+    <template>#{segment_1} #{segment_2}</template>
+  </category>
+</aiml>
+"
+
+# Imagined a compromise path that is neither ideal or tragic.
+open("_imaginedpath/brains/nuetral_outcome.aiml", "w") { |f|
+  segment_1 = character_fate[1].strip
+  segment_2 = dating_outcome[0].strip
+
+  f.puts aiml
+}
