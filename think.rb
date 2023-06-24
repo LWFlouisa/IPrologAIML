4e6bf58f7132943dbd1073fba18e6391939900b0
Sarah Weaver <9245433+LWFlouisa@users.noreply.github.com>
2023-06-24 18:13:22 -0400

    Update think.rb

--- think.rb
+++ think.rb
@@ -5,19 +5,19 @@ dating_outcome = File.readlines("_narratives/outcomes/dating_outcomes.txt")
 # Make the total imagined branch the size of the darkest path.
 # branch_size = branch_1.size.to_i
 
-aiml = "<?xml version = "1.0" encoding = 'UTF-8'?>
-<aiml version = "1.0.1" encoding = 'UTF-8'>
+# Imagined a compromise path that is neither ideal or tragic.
+open("_imaginedpath/brains/nuetral_outcome.aiml", "w") { |f|
+  segment_1 = character_fate[1].strip
+  segment_2 = dating_outcome[0].strip
+
+  aiml = "<?xml version = '1.0' encoding = 'UTF-8'?>
+<aiml version = '1.0.1' encoding = 'UTF-8'>
   <category>
     <pattern>Can you tell me the nuetral path within this dataset?</pattern>
     <template>#{segment_1} #{segment_2}</template>
   </category>
 </aiml>
 "
-
-# Imagined a compromise path that is neither ideal or tragic.
-open("_imaginedpath/brains/nuetral_outcome.aiml", "w") { |f|
-  segment_1 = character_fate[1].strip
-  segment_2 = dating_outcome[0].strip
 
   f.puts aiml
 }
